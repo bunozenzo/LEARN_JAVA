882019cc487fa95d3e53d0cd06aab4e06315bd9c
bunozenzo <dangbon313@gmail.com>
2024-10-14 00:21:22 +0700

    Lesson 7

---
+++ LearnJavaCore/src/Lesson7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Lesson7 {
+    public static void main(String[] args) {
+        //Bài 7: Viết chương trình tính giai thừa của một số nguyên dương nhập từ bàn phím.
+        Scanner s = new Scanner(System.in);
+        System.out.println("Nhap thong tin");
+        int n = s.nextInt();
+        System.out.println("Gia thua: " + factorial(n));
+    }
+
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 1; i < n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
