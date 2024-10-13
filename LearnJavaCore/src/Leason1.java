6f0aaea7a97219de855947b40cb2a03720292af4
bunozenzo <dangbon313@gmail.com>
2024-10-13 23:45:08 +0700

    Lesson 2

--- LearnJavaCore/src/Leason1.java
+++ LearnJavaCore/src/Leason1.java
@@ -1,5 +1,6 @@
 public class Leason1 {
     public static void main(String[] args) {
+        // Bài 1: Viết chương trình in ra “Hello, World!”.
         System.out.println("hello, I am bondm1");
     }
 }
