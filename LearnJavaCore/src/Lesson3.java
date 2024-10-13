984ba098d49e1da80e74804f2d979558d50ed3e4
bunozenzo <dangbon313@gmail.com>
2024-10-13 23:56:13 +0700

    Lesson 3

---
+++ LearnJavaCore/src/Lesson3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        //Bài 3: Viết chương trình kiểm tra xem một số là số chẵn hay số lẻ.
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter number:");
+        int number = s.nextInt();
+        System.out.println("Số này là số " + IsEven(number));
+    }
+
+    public static String IsEven(int number) {
+        int a = number % 2;
+        return a == 0 ? "Chẵn" : "Lẻ";
+    }
+}
